a0de0932c3e0186f80566590db5bf6d950cdb6f4
Oleksii Grachov <827984+yanzay@users.noreply.github.com>
2026-08-18 23:30:39 +0300

    Update NeoAnki2 to 1.0.278

--- Casks/neoanki2.rb
+++ Casks/neoanki2.rb
@@ -1,11 +1,11 @@
 cask "neoanki2" do
-  version "1.0.277"
-  sha256 "696137ccaa35f39339a98d91c531c08e61e30b20a4fab4b376c7cc7beccb86ed"
+  version "1.0.278"
+  sha256 "ae9de0616fa8d208f28a2fae8029ec85ec9df53db70816f13a820e075deebd50"
 
   url "https://github.com/neoanki2/neoanki2/releases/download/v#{version}/NeoAnki2-#{version}-mac-universal.dmg"
   name "NeoAnki2"
   desc "Native, local-first spaced-repetition app with FSRS scheduling"
-  homepage "https://neoanki2.github.io/neoanki2/"
+  homepage "https://neoanki2.github.io/"
 
   depends_on macos: :sonoma
 
